c4ed2e02c8f1c8129550b78b939f17043ed78a51
taihochan <jake831210@gmail.com>
2023-08-07 11:16:00 +0800

    Fix: self-intro 改為 hash mode

--- Components/self-intro/vite.config.ts
+++ Components/self-intro/vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
-const baseURL = process.env.NODE_ENV === 'production' ? '/<REPO>/self-intro/' : './';
+const baseURL = process.env.NODE_ENV === 'production' ? '/Components/self-intro/' : './';
 
 // https://vitejs.dev/config/
 export default defineConfig({
