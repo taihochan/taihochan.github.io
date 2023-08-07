11be81846a51ab2690ffc82e72709697116c51ed
taihochan <jake831210@gmail.com>
2023-08-07 11:21:26 +0800

    測試vite部署

--- Components/self-intro/vite.config.ts
+++ Components/self-intro/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: baseURL,
+  base: '/Components/self-intro/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
